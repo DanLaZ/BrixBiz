adb40edf83e0994d431f6497d1d35ec65f9acc77
Gabitec <anagubi@gmail.com>
2019-08-05 22:59:12 -0400

    Abby'sfile

--- server.js
+++ server.js
@@ -1,94 +1,56 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-var passport  = require('passport')
-var session   = require('express-session')
-var bodyParser = require('body-parser')
-var db = require("./models");
+var passport = require("passport");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var models = require("./models");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
-
-// Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
-
-var syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
-
-module.exports = app;
-
-var app = express()
-//For BodyParser
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
 
 app.use(express.urlencoded({extended: false}))
 // For Passport
  
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(session({ secret: "keyboard cat",resave: true, saveUninitialized:true})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
-app.use(express.json())
+app.use(express.json());
 app.use(express.static("./app/public"));
 
 //For Handlebars
-app.engine('handlebars', exphbs({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main'
+app.engine("handlebars", exphbs({
+    layoutsDir: "views/layouts",
+    defaultLayout: "main"
 }));
-app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-    res.send('Welcome to Passport with Sequelize');
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+    res.send("Welcome to Passport with Sequelize");
 });
 
-//Models
-var models = require("./app/models");
-
 //Routes
-require('./app/routes/auth.js')(app,passport);
-
+require("./routes/auth.js")(app,passport);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 //load passport strategies
-require('./app/config/passport/passport.js')(passport, models.user);
- 
+require("./config/passport/passport.js")(passport, models.user);
+
+var PORT = process.env.PORT || 3000;
+
 //Sync Database
 models.sequelize.sync().then(function() {
-    console.log('Nice! Database looks fine')
-    app.listen(5000, function(err) {
+    console.log("Nice! Database looks fine")
+    app.listen(PORT, function(err) {
         if (!err)
             console.log("Site is live");
         else console.log(err)
@@ -96,6 +58,5 @@ models.sequelize.sync().then(function() {
 }).catch(function(err) {
     console.log(err, "Something went wrong with the Database Update!")
 });
- 
 
 module.exports = app;
