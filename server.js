e7030add0c6d2b041453afb0b556e787e897f301
Abigail Ettore <aettore1@fordham.edu>
2019-08-05 20:40:24 -0400

    abby's push

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var passport  = require('passport')
+var session   = require('express-session')
+var bodyParser = require('body-parser')
 var db = require("./models");
 
 var app = express();
@@ -43,5 +45,57 @@ db.sequelize.sync(syncOptions).then(function() {
     );
   });
 });
+
+module.exports = app;
+
+var app = express()
+//For BodyParser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use(express.urlencoded({extended: false}))
+// For Passport
+ 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(express.json())
+app.use(express.static("./app/public"));
+
+//For Handlebars
+app.engine('handlebars', exphbs({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+    res.send('Welcome to Passport with Sequelize');
+});
+
+//Models
+var models = require("./app/models");
+
+//Routes
+require('./app/routes/auth.js')(app,passport);
+
+
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+    app.listen(5000, function(err) {
+        if (!err)
+            console.log("Site is live");
+        else console.log(err)
+    });
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+ 
 
 module.exports = app;
